5ef9020c2af46433be6b16dc7c495765a105eedc
Monica M Nikolaisen <70950983+mmunkvold@users.noreply.github.com>
2022-03-12 18:10:31 +0100

    Update featured.js

--- js/components/featured.js
+++ js/components/featured.js
@@ -3,19 +3,19 @@ export default function renderFeatured(products) {
   featuredContainer.innerHTML = "";
 
   products.forEach((product) => {
-    const imageUrl = "https://under-my-umbrella.herokuapp.com";
+    //const imageUrl = "https://under-my-umbrella.herokuapp.com";
     const { id, title, price, image } = product;
 
     if (product.featured) {
       featuredContainer.innerHTML += `
       <div class="featured__product">
         <a class="featured__link" href="detail.html?id=${id}">
-          <div style='background-image: url("${imageUrl}${image.url}")' class="featured__img"></div>
+          <div style='background-image: url("${image.url}")' class="featured__img"></div>
           <div class="featured__txt">
           <h2>${title}</h2></div>
         </a>
         <div class="center--btn">
-          <button class="cart__btn" data-id="${id}" data-title="${title}" data-price="${price}" data-image="${imageUrl}${
+          <button class="cart__btn" data-id="${id}" data-title="${title}" data-price="${price}" data-image="${
         image.url
       }"><i class="icon bi bi-basket2"></i>${price.toFixed(2)}
           </button>
