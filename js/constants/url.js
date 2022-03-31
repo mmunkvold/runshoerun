76a80db83e68dcd1d6c4bd1d16e7a5655976be59
Monica M Nikolaisen <70950983+mmunkvold@users.noreply.github.com>
2022-03-31 12:03:01 +0200

    Update url.js

--- js/constants/url.js
+++ js/constants/url.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://under-my-umbrella.herokuapp.com/";
+//export const BASE_URL = "https://under-my-umbrella.herokuapp.com/";
+export const BASE_URL ="http://localhost:1337/"
 export const LOGIN_URL = BASE_URL + "auth/local";
